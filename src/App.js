bc49e4529547d5cc6f9647dbe81799588ebad20f
Ben Rehmann <brehmann@me.com>
2024-07-01 09:57:36 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <label className='hamburgerMenu'>
+        <input type='checkbox' />
+      </label>
+      <aside className='sidebar'>
+        <nav>
+          <div>This</div>
+          <div>is</div>
+          <div>the</div>
+          <div>Sidebar</div>
+        </nav>
+      </aside>
+
+
     </div>
   );
 }
